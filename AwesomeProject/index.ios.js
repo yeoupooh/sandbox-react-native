95c1331208708aef181e117306463d2fde60f712
Thomas Min <thomas.min.git@gmail.com>
2016-05-26 22:10:48 +0900

    Add listview tutorial

--- AwesomeProject/index.ios.js
+++ AwesomeProject/index.ios.js
@@ -5,47 +5,119 @@
 
 import React, {
   AppRegistry,
+  Image,
+  ListView,
   Component,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
+/**
+ * For quota reasons we replaced the Rotten Tomatoes' API with a sample data of
+ * their very own API that lives in React Native's Github repo.
+ */
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 class AwesomeProject extends Component {
+
+  constructor(props) {
+   super(props);
+   this.state = {
+     dataSource: new ListView.DataSource({
+       rowHasChanged: (row1, row2) => row1 !== row2,
+     }),
+     loaded: false,
+   };
+ }
+
+ componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
+        });
+      })
+      .done();
+  }
+
   render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={styles.listView}
+      />
     );
   }
+
+ renderLoadingView() {
+   return (
+     <View style={styles.container}>
+       <Text>
+         Loading movies...
+       </Text>
+     </View>
+   );
+ }
+
+ renderMovie(movie) {
+   return (
+     <View style={styles.container}>
+       <Image
+         source={{uri: movie.posters.thumbnail}}
+         style={styles.thumbnail}
+       />
+       <View style={styles.rightContainer}>
+         <Text style={styles.title}>{movie.title}</Text>
+         <Text style={styles.year}>{movie.year}</Text>
+       </View>
+     </View>
+   );
+ }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  rightContainer: {
+   flex: 1,
+ },
+ title: {
     fontSize: 20,
+    marginBottom: 8,
     textAlign: 'center',
-    margin: 10,
   },
-  instructions: {
+  year: {
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
   },
 });
 
